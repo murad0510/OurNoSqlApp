f4af804d8e34f00d754c0d19293c1415317cf167
murad0510 <114216562+murad0510@users.noreply.github.com>
2024-04-16 18:11:04 +0400

    Add project files.

---
+++ AzureStorageLibrary/Models/Player.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Azure.Cosmos.Table;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureStorageLibrary.Models
+{
+    public class Player : TableEntity
+    {
+        public string? Name { get; set; }
+        public string? Surname { get; set; }
+        public decimal Salary { get; set; }
+        public int Score { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
---
+++ MVCWebApp/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using AzureStorageLibrary;
+using AzureStorageLibrary.Models;
+using Microsoft.AspNetCore.Mvc;
+using MVCWebApp.Models;
+using System.Diagnostics;
+
+namespace MVCWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly INoSqlStorage<Player> _noSqlStorage;
+
+        public HomeController(INoSqlStorage<Player> noSqlStorage)
+        {
+            _noSqlStorage = noSqlStorage;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var player = new Player
+            {
+                RowKey = Guid.NewGuid().ToString(),
+                PartitionKey = "Young",
+                Name = "Ehned",
+                Surname = "Memmedov",
+                BirthDate = DateTime.Now,
+                Salary = 1200,
+                Score = 100
+            };
+            await _noSqlStorage.Add(player);
+            ViewBag.products = (await _noSqlStorage.All()).ToList();
+            return View();
+        }
+    }
+}
